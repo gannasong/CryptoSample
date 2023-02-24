18a6265c23c63c4ed4bb96a81aee07163bd1cd42
miguel <gannasong@gmail.com>
2023-02-24 20:33:39 +0800

    Initial Commit

---
+++ CryptoSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoSample
+//
+//  Created by SUNG HAO LIN on 2023/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoSampleUITests/CryptoSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CryptoSampleUITestsLaunchTests.swift
+//  CryptoSampleUITests
+//
+//  Created by SUNG HAO LIN on 2023/2/24.
+//
+
+import XCTest
+
+final class CryptoSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
